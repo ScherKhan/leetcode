c8611458941fd230a06dbf5e93570f0f948938f2
Vladimir Koltunov <realredbaron@tut.by>
2021-04-08 16:32:22 +0300

    FIrst simple solution for Apr 8's problem

--- Contests/April 2021 Challenge/apr8_letterCombinations.py
+++ Contests/April 2021 Challenge/apr8_letterCombinations.py
@@ -8,14 +8,32 @@ Note that 1 does not map to any letters.
 """
 from typing import List
 
+mapping = {2: 'abc', 
+           3: 'def',
+           4: 'ghi', 
+           5: 'jkl', 
+           6: 'mno',
+           7: 'pqrs', 
+           8: 'tuv', 
+           9: 'wxyz'}
+
 def letterCombinations(digits: str) -> List[str]:
-    pass
+    combs = []
+    for digit in digits:
+        letters = mapping[int(digit)]
+        if len(combs) == 0:
+            combs = [l for l in letters]
+        else:
+            combs = [comb + l for comb in combs for l in letters]
+
+    return combs
 
 
 def main():
+    print(letterCombinations('23'))
     assert letterCombinations('23') == ["ad","ae","af","bd","be","bf","cd","ce","cf"]
     assert letterCombinations('') == [], 'Empty input'
-    assert letterCombinations('2') == ['a', 'b', 'c'], 'Sinble digit'
+    assert letterCombinations('2') == ['a', 'b', 'c'], 'Single digit'
     print('All tests passed')
 
 
