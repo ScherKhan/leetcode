d4b85dbb91e28dbb41417cfb8053a71f0d17b551
Vladimir Koltunov <realredbaron@tut.by>
2021-04-09 17:58:57 +0300

    First simple solution for Apr 9's problem using translation to regular English alphabet

--- Contests/April 2021 Challenge/apr9_isAlienSorted.py
+++ Contests/April 2021 Challenge/apr9_isAlienSorted.py
@@ -6,14 +6,21 @@ Given a sequence of words written in the alien language, and the order of the al
  if the given words are sorted lexicographicaly in this alien language.
 """
 from typing import List
-
+from string import ascii_lowercase
 
 def isAlienSorted(words: List[str], order: str) -> bool:
-    pass
+    alphabet = ascii_lowercase[:len(order)]
+    translated = [''.join(map(lambda x: alphabet[order.index(x)], word)) for word in words]
+    #print(translated, sorted(translated))
+    return translated == sorted(translated)
 
 
 def main():
     print(isAlienSorted(words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"))
+    assert isAlienSorted(["hello","leetcode"], "hlabcdefgijkmnopqrstuvwxyz") == True
+    assert isAlienSorted(["word","world","row"], "worldabcefghijkmnpqstuvxyz") == False
+    assert isAlienSorted(["apple","app"], "abcdefghijklmnopqrstuvwxyz") == False
+    assert isAlienSorted(["kuvp", "q"], "ngxlkthsjuoqcpavbfdermiywz") == True
     print('All tests passed')
 
 
