6de2538e02f8966d2f5508ed3c5b33d5a62f3326
Vladimir Koltunov <realredbaron@tut.by>
2021-04-15 11:41:17 +0300

    Add template for Apr 15's problem: find Fibonacci number

---
+++ Contests/April 2021 Challenge/apr15_fibonacciNumber.py
@@ -0,0 +1,27 @@
+"""
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number
+ is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+"""
+import time
+
+
+def fib(n: int) -> int:
+    pass
+
+
+def main():
+    start_time = time.time()
+    assert fib(0) == 0
+    assert fib(1) == 1
+    assert fib(30) == 832040
+    assert fib(15) == 610
+    assert fib(4) == 3
+    print(f'All tests passed. Took {time.time() - start_time:.3f} seconds')
+
+
+if __name__ == '__main__':
+    main()
